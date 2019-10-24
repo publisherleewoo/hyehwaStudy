72f5c53e3c638276a505d6bd0bcc702c8bb283d7
leewoo <dlguddn3214@naver.com>
2019-10-24 21:28:31 +0900

    empty

--- 191030/router/index.js
+++ 191030/router/index.js
@@ -1,11 +1,13 @@
 var express = require('express')
 var router = express.Router();
 var path = require("path")
-var main = require("./main")
-var email = require("./email")
+var main = require("./main/main")
+var email = require("./email/email")
+var join = require("./join/join")
 
 router.use("/main",main)
 router.use("/email",email)
+router.use("/join",join)
 
 router.get('/', function (req, res) {
     console.log('main is loaded')
